356d1bfbc6177ebc1d808ac98f7dbba7de5f5189
Thomas Felix <thomas.felix@tfelix.de>
2015-02-22 10:59:05 +0100

    BoxCollisionShape hinzugefügt.

--- bestia-core/src/main/java/net/bestia/core/game/zone/CollisionShape.java
+++ bestia-core/src/main/java/net/bestia/core/game/zone/CollisionShape.java
@@ -1,7 +1,9 @@
 package net.bestia.core.game.zone;
 
+
 public abstract class CollisionShape {
 
 	public abstract boolean collide(CollisionShape shape);
 	public abstract boolean collide(Point p);
+	public abstract Dimension getBoundingBox();
 }
