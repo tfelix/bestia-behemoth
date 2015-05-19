d6d3eb2cbbe336a8ff49fdb07403ba7a9d43c2b4
Thomas Felix <thomas.felix@tfelix.de>
2015-05-19 20:17:01 +0200

    Interserver lauffähig gemacht.

--- bestia-connect/src/main/java/net/bestia/connect/Interserver.java
+++ bestia-connect/src/main/java/net/bestia/connect/Interserver.java
@@ -1,5 +1,7 @@
 package net.bestia.connect;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.zeromq.ZMQ;
@@ -9,6 +11,41 @@ import org.zeromq.ZMQ.Socket;
 public class Interserver {
 
 	private final static Logger log = LogManager.getLogger(Interserver.class);
+	private final static int PORT = 9999;
+
+	private static class MQThread extends Thread {
+		
+		public AtomicBoolean isRunning = new AtomicBoolean(true);
+		private final static String NAME = "MQTTHREAD";
+		
+		public MQThread() {
+			this.setName(NAME);
+		}
+		
+		public void run() {
+
+			final Context context = ZMQ.context(1);
+
+			final Socket publisher = context.socket(ZMQ.PUB);
+			publisher.bind("tcp://*:" + PORT);
+
+			while (isRunning.get()) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// no op.
+				}
+			}
+			
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				public void run() {
+					log.info("Terminating the Interserver...");
+					publisher.close();
+					context.term();
+				}
+			});
+		}
+	};
 
 	public static void main(String[] args) {
 
@@ -16,25 +53,11 @@ public class Interserver {
 
 		// TODO Replace with build file version.
 		log.info("Version v" + "0.0.1-alpha");
-
-		Context context = ZMQ.context(1);
-
-		Socket publisher = context.socket(ZMQ.PUB);
-		// TODO Port wählbar machen.
-		publisher.bind("tcp://*:9999");
-
-		while (!Thread.currentThread().isInterrupted()) {
-			// Write two messages, each with an envelope and content
-			publisher.sendMore("A");
-			publisher.send("We don't want to see this");
-			publisher.sendMore("B");
-
-			publisher.send("We would like to see this");
-			log.info("Geht.");
-		}
-		publisher.close();
-		context.term();
-
+		
+		final MQThread mqt = new MQThread();
+		mqt.start();
+		
+		log.info("Interserver started on: .");
 	}
 
 }
