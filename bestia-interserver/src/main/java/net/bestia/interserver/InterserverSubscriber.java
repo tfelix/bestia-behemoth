7c906eb8a52ebc5a8a76081d616fbde75a06d910
Thomas Felix <thomas.felix@tfelix.de>
2015-06-04 14:07:21 +0200

    Final.

--- bestia-interserver/src/main/java/net/bestia/interserver/InterserverSubscriber.java
+++ bestia-interserver/src/main/java/net/bestia/interserver/InterserverSubscriber.java
@@ -1,13 +1,38 @@
 package net.bestia.interserver;
 
+/**
+ * Used by classes to connect to the interserver in a subscribing way. Incoming messages will be received via topics to
+ * which one can subscribe. Depending on the arriving messages these topics will be triggered.
+ * 
+ * @author Thomas Felix <thomas.felix@tfelix.de>
+ *
+ */
 public interface InterserverSubscriber {
 
+	/**
+	 * Connects to the interserver.
+	 */
 	public abstract void connect();
 
+	/**
+	 * Disconnects from the interserver.
+	 */
 	public abstract void disconnect();
 
+	/**
+	 * This will subscribe the message system to a topic from which we will receive notations and data.
+	 * 
+	 * @param topic
+	 *            The topic to subscribe to.
+	 */
 	public abstract void subscribe(String topic);
 
+	/**
+	 * Unsubscribe from the given topic.
+	 * 
+	 * @param topic
+	 *            The topic to unsubscribe to.
+	 */
 	public abstract void unsubscribe(String topic);
 
 }
