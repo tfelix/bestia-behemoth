9ade9a9d97a4195c9dd491d6340eae6d9cd64d7d
tfelix <tfelix@fallendream.com>
2015-08-20 18:30:23 +0200

    Unnötig gewordene Klassen entfernt.

--- bestia-messages/src/main/java/net/bestia/messages/api/AccountCheckJson.java
+++ bestia-messages/src/main/java/net/bestia/messages/api/AccountCheckJson.java
@@ -1,9 +1,15 @@
 package net.bestia.messages.api;
 
+/**
+ * Small helper object to send results of the account check to the client via
+ * JSON.
+ * 
+ * @author Thomas Felix <thomas.felix@tfelix.de>
+ *
+ */
 public class AccountCheckJson {
 
 	public boolean emailUsed = false;
 	public boolean nameUsed = false;
-	
-	
+
 }
