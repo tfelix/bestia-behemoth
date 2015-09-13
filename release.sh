e385d8b4d37307008abd5bc6b678e914fbb0616d
Thomas Felix <thomas.felix@tfelix.de>
2015-09-13 03:28:09 +0200

    Änderungen am Release Script

--- release.sh
+++ release.sh
@@ -21,14 +21,18 @@ read -p "Enter the new version (like v.1.2.3): " version
 
 git checkout master
 git merge $current_version || error_exit
+mvn versions:set -DnewVersion=$current_version
+git add .
+git commit -m "$current_version release."
+git tag -a v$current_version -m "$current_version release."
 
 # Create a new versioning branch.
 git checkout -b $version
-mvn versions:set -DnewVersion=$version
+mvn versions:set -DnewVersion=$version-SNAPSHOT
 git add .
 git commit -m "$version start"
 
 # Push changes to the master.
-git push origin master
+git push --follow-tags
 git push origin $version
 
