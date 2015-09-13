61cdd225d6a198baf4bd200a97fc3a2765f60441
Thomas Felix <thomas.felix@tfelix.de>
2015-09-13 03:12:42 +0200

    Kleines Helper Script zum Erstellen neuer Versionen.

---
+++ release.sh
@@ -0,0 +1,34 @@
+@echo off
+
+function error_exit
+{
+	echo "Failed to create the release version."
+	exit 1
+}
+
+echo This will release a new version of the Bestia Behemoth software system.
+echo 
+echo Testing release...
+
+mvn test || error_exit
+
+# get the current version.
+current_version = $(git rev-parse --abbrev-ref HEAD)
+
+read -p "Enter the new version (like v.1.2.3): " version
+
+# do the merging of the current version.
+
+git checkout master
+git merge $current_version || error_exit
+
+# Create a new versioning branch.
+git checkout -b $version
+mvn versions:set -DnewVersion=$version
+git add .
+git commit -m "$version start"
+
+# Push changes to the master.
+git push origin master
+git push origin $version
+
