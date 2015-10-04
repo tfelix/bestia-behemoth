32ca616c61bb4fcd397448b218cac222786f9c58
Thomas Felix <thomas.felix@tfelix.de>
2015-10-04 18:20:21 +0200

    Remove Componente hinzugefügt.

---
+++ bestia-zoneserver/src/main/java/net/bestia/zoneserver/ecs/component/DelayedRemove.java
@@ -0,0 +1,23 @@
+package net.bestia.zoneserver.ecs.component;
+
+import com.artemis.Component;
+
+/**
+ * Each entity with such an compoenent will be removed after the delay has
+ * expired. Delay is in ms.
+ * 
+ * @author Thomas Felix <thomas.felix@tfelix.de>
+ *
+ */
+public class DelayedRemove extends Component {
+
+	public int removeDelay;
+
+	public DelayedRemove() {
+
+	}
+
+	public DelayedRemove(int delay) {
+		this.removeDelay = delay;
+	}
+}
