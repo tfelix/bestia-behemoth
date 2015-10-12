065f78dca94e7b0644569d93ec5760201658fee4
Thomas Felix <thomas.felix@tfelix.de>
2015-10-12 21:46:02 +0200

    Kommentare.

--- bestia-models/src/main/java/net/bestia/model/domain/I18n.java
+++ bestia-models/src/main/java/net/bestia/model/domain/I18n.java
@@ -14,6 +14,14 @@ import javax.persistence.Transient;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+/**
+ * Saves translations for the bestia game into the database. This table is used
+ * to fetch all kinds of hot-loaded, dynamic translations for the bestia game
+ * system like quests, attack descriptions and item names etc.
+ * 
+ * @author Thomas Felix <thomas.felix@tfelix.de>
+ *
+ */
 @Entity
 @Table(name = "i18ns")
 public class I18n implements Serializable {
@@ -27,7 +35,7 @@ public class I18n implements Serializable {
 		@Transient
 		private static final long serialVersionUID = 1L;
 		protected String key;
-		
+
 		@Enumerated(EnumType.STRING)
 		protected TranslationCategory category;
 
@@ -35,8 +43,11 @@ public class I18n implements Serializable {
 		@Column(length = 5)
 		protected String lang;
 
+		/**
+		 * Std. Ctor for Hibernate.
+		 */
 		public I18nPK() {
-
+			// no op.
 		}
 
 		public I18nPK(TranslationCategory category, String key, String lang) {
@@ -61,16 +72,36 @@ public class I18n implements Serializable {
 	private I18nPK id;
 
 	private String value;
-	
+
+	/**
+	 * Std. Ctor for Hibernate.
+	 */
 	public I18n() {
-		
+		// no op.
 	}
-	
+
+	/**
+	 * Ctor for setting up an translation entry.
+	 * 
+	 * @param key
+	 *            The identifier key of the translation.
+	 * @param category
+	 *            The category to break translation further down.
+	 * @param lang
+	 *            The language code of the translation.
+	 * @param value
+	 *            The actual translation.
+	 */
 	public I18n(String key, TranslationCategory category, String lang, String value) {
 		id = new I18nPK(category, key, lang);
 		this.value = value;
 	}
 
+	/**
+	 * The translation text.
+	 * 
+	 * @return The text for the translation.
+	 */
 	public String getValue() {
 		return value;
 	}
