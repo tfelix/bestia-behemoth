3faaf34795dffa388a4a33cd4894e924a2cec932
tfelix <thomas.felix@tfelix.de>
2015-10-23 23:37:40 +0200

    Tests kompilieren wieder.

--- bestia-models/src/test/java/net/bestia/model/domain/LocationTest.java
+++ bestia-models/src/test/java/net/bestia/model/domain/LocationTest.java
@@ -11,46 +11,6 @@ public class LocationTest {
 		Assert.assertEquals(5, loc.getX());
 		Assert.assertEquals(10, loc.getY());
 		Assert.assertEquals("test", loc.getMapDbName());
-		Assert.assertFalse(loc.hasChanged());
-	}
-	
-	@Test
-	public void not_changed_flag_test() {
-		Location loc = new Location("test", 10, 10);
-		loc.getMapDbName();
-		loc.getX();
-		loc.getY();
-		Assert.assertFalse(loc.hasChanged());
-	}
-	
-	@Test
-	public void changed_and_reset_mabdbname_test() {
-		Location loc = new Location("test", 10, 10);
-		
-		loc.setMapDbName("test123");
-		Assert.assertTrue("Flag is not set to changed.", loc.hasChanged());
-		loc.resetChanged();
-		Assert.assertFalse("Flag is not reset to unchanged.", loc.hasChanged());
-	}
-	
-	@Test
-	public void changed_and_reset_x_test() {
-		Location loc = new Location("test", 10, 10);
-		
-		loc.setX(5);
-		Assert.assertTrue("Flag is not set to changed.", loc.hasChanged());
-		loc.resetChanged();
-		Assert.assertFalse("Flag is not reset to unchanged.", loc.hasChanged());
-	}
-	
-	@Test
-	public void changed_and_reset_y_test() {
-		Location loc = new Location("test", 10, 10);
-		
-		loc.setY(11);
-		Assert.assertTrue("Flag is not set to changed.", loc.hasChanged());
-		loc.resetChanged();
-		Assert.assertFalse("Flag is not reset to unchanged.", loc.hasChanged());
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
--- bestia-zoneserver/src/main/java/net/bestia/zoneserver/ecs/component/Position.java
+++ bestia-zoneserver/src/main/java/net/bestia/zoneserver/ecs/component/Position.java
@@ -2,18 +2,21 @@ package net.bestia.zoneserver.ecs.component;
 
 import com.artemis.Component;
 
+import net.bestia.zoneserver.zone.shape.Vector2;
+
 public class Position extends Component {
 	
-	public int x;
-	public int y;
+	public Vector2 position;
 	
 	public Position() {
 		
 	}
 	
 	public Position(int x, int y) {
-		this.x = x;
-		this.y = y;
+		setPosition(x, y);
 	}
-
+	
+	public void setPosition(int x, int y) {
+		position = new Vector2(x, y);
+	}
 }
