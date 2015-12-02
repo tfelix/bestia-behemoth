d93e3f7554fffef4759831d624a3f205ac55d8e1
Thomas Felix <thomas.felix@tfelix.de>
2015-12-02 21:41:36 +0100

    Anpassung des boot States.

--- bestia-www-client/src/js/engine/states/boot_state.js
+++ bestia-www-client/src/js/engine/states/boot_state.js
@@ -1,12 +1,21 @@
 /**
- * State is triggered once when the game starts.
+ * State is triggered once when the game starts. It will preload all the really 
+ * needed assets in order to to a proper loading screen and do some basic game setup.
  * 
  * @constructor
  * @class Bestia.Engine.States.BootState
  */
 Bestia.Engine.States.BootState = function() {
 
+	// no op.
+	
+};
 
+/**
+ * Preload all the needed assets in order to display a loading screen.
+ */
+Bestia.Engine.States.BootState.prototype.preload = function() {
+	
 };
 
 Bestia.Engine.States.BootState.prototype.create = function() {
