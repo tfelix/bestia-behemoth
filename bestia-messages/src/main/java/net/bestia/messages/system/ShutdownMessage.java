07608d3f3241a4054138f38623caf7b1c2004198
Thomas Felix <thomas.felix@tfelix.de>
2016-03-07 23:08:58 +0100

    Shutdown Message optimiert.

--- bestia-messages/src/main/java/net/bestia/messages/system/ShutdownMessage.java
+++ bestia-messages/src/main/java/net/bestia/messages/system/ShutdownMessage.java
@@ -1,21 +1,50 @@
 package net.bestia.messages.system;
 
-import net.bestia.messages.Message;
+import net.bestia.messages.ZoneserverMessage;
 
-public class ShutdownMessage extends Message {
-	
+/**
+ * A message which will upon receiving shut down the server.
+ * 
+ * @author Thomas Felix <thomas.felix@tfelix.de>
+ *
+ */
+public class ShutdownMessage extends ZoneserverMessage {
+
 	private static final long serialVersionUID = 1L;
 	public static final String MESSAGE_ID = "system.shutdown";
 
-	@Override
-	public String getMessageId() {
-		return MESSAGE_ID;
+	private String messagePath = null;
+
+	public ShutdownMessage(String receiverServerName, String senderServerName) {
+		super(receiverServerName, senderServerName);
+	}
+
+	/**
+	 * Creates a {@link ShutdownMessage} which is directed to ALL zoneservers to
+	 * shut them all down.
+	 * 
+	 * @return A shutdown message which is broadcasted to all server.
+	 */
+	public static ShutdownMessage getShutdownBroadcast() {
+		// Create a new message but set the message path to the broadcast path.
+		final ShutdownMessage msg = new ShutdownMessage("broadcast", "behemoth");
+		msg.messagePath = msg.getServerBroadcastPath();
+
+		return msg;
 	}
 
 	@Override
 	public String getMessagePath() {
-		// TODO Auto-generated method stub
-		return null;
+		if (messagePath != null) {
+			return messagePath;
+		} else {
+			return super.getMessagePath();
+		}
+	}
+
+	@Override
+	public String getMessageId() {
+		return MESSAGE_ID;
 	}
 
 }
