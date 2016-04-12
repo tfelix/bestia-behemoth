a4e716c2fb8c4af99806b33f7330f6486114f0c0
Thomas Felix <thomas.felix@tfelix.de>
2016-04-12 23:30:19 +0200

    Weitere Klassen auf ES6 umstellen.

---
+++ bestia-www-client/src/js/engine/core/DemandLoader.js
@@ -0,0 +1,245 @@
+import NOOP from '../../util/NOOP.js';
+
+/**
+ * Performs and manages on demand reloads for assets. If something has to be
+ * loaded it can be handed over to this class. This will check via the phaser
+ * Loader if the object already exists, if one can ask the on demand loader to
+ * load it with a given callback function. After loading has completed the
+ * function will be executed once.
+ * 
+ * @param {Phaser.Loader}
+ *            loader - Reference to the phaser loader since we must hook into
+ *            that one.
+ */
+export default class DemandLoader {
+	
+	constructor(loader, cache, urlHelper) {
+
+	if (!(loader instanceof Phaser.Loader)) {
+		throw "DemandLoader: Loader is not a Phaser.Loader";
+	}
+
+	if (!(cache instanceof Phaser.Cache)) {
+		throw "DemandLoader: Cache is not a Phaser.Cache";
+	}
+
+	if (!urlHelper) {
+		throw "UrlHelper: Can not be null";
+	}
+
+	this._loader = loader;
+	this._phaserCache = cache;
+	this._urlHelper = urlHelper;
+
+	this._cache = {};
+	this._keyCache = {};
+
+	// Add the callbacks.
+	loader.onFileComplete.add(this._fileLoadedCallback, this);
+	}
+	
+	_fileLoadedCallback(progress, key) {
+
+		var cacheData = null;
+
+		if (this._cache.hasOwnProperty(key)) {
+			cacheData = this._cache[key];
+		} else if (this._keyCache.hasOwnProperty(key)) {
+			// Go the indirection.
+			cacheData = this._cache[this._keyCache[key]];
+			delete this._keyCache[key];
+		} else {
+			// No cache entry found. Propably the file was directly loaded without
+			// the use of the demand loader.
+			// Skip the callback search.
+			return;
+		}
+
+		if (cacheData.type === 'pack' && cacheData.toLoad === 0) {
+			// Beginning of the pack load.
+			var pack = this._phaserCache.getJSON(key);
+
+			// Add all files of this pack to our file list.
+			var keyList = pack[key].map(function(x) {
+				return x.key;
+			});
+
+			keyList.forEach(function(x) {
+				this._keyCache[x] = key;
+			}, this);
+
+			this._cache[key].items = keyList;
+			this._cache[key].toLoad = keyList.length;
+
+			// Start to load all data in this pack.
+			this._loader.pack(key, undefined, pack);
+			this._loader.start();
+		} else {
+			cacheData.toLoad--;
+			if (cacheData.toLoad === 0) {
+
+				cacheData.callbackFns.forEach(function(x) {
+					try {
+						x();
+					} catch (err) {
+						console.error("DemandLoader#_fileLoadedCallback: " + err);
+					}
+				});
+
+				delete this._cache[key];
+				// Restart to fetch queued stuff.
+				this._loader.start();
+			}
+		}
+	}
+
+	/**
+	 * Checks if all the given keys are already inside the cache. Keys can be a
+	 * single string or an array of strings containing the the key names.
+	 * 
+	 * @param keys
+	 *            {string|array}
+	 */
+	has(keys, type) {
+		if (Array.isArray(keys)) {
+
+			var hasCache = true;
+
+			keys.forEach(function(val) {
+				hasCache = hasCache & this._hasType(val.key, val.type);
+			}.bind(this));
+
+			return hasCache;
+
+		} else {
+			if (type === undefined) {
+				return false;
+			}
+
+			return this._hasType(keys, type);
+		}
+	}
+
+	get(key, type) {
+
+		if (!this._hasType(key, type)) {
+			return null;
+		}
+
+		switch (type) {
+		case 'item':
+			return this._phaserCache.getImage(key);
+		case 'json':
+			return this._phaserCache.getJSON(key);
+		default:
+			console.warn("DemandLoader#get: Unknown type.");
+			return false;
+		}
+	}
+
+	_hasType(key, type) {
+		switch (type) {
+		case 'item':
+			return this._phaserCache.checkImageKey(key);
+		case 'json':
+			return this._phaserCache.checkJSONKey(key);
+		default:
+			console.warn("_hasType: Unknown type.");
+			return false;
+
+		}
+	}
+
+	loadPackData(pack, fnOnComplete) {
+
+		// If there is currently a loading in progress we can not insert our pack
+		// data. This will mess up the Phaser Loader. We MUST wait until the loading
+		// has finished. DOES NOT WORK.
+		/*
+		 * if (this._loader.isLoading) { this._loadPackCallBuffer.push(function() {
+		 * this.loadPackData(pack, fnOnComplete); }.bind(this)); return; }
+		 */
+
+		fnOnComplete = fnOnComplete || NOOP;
+
+		// Get the key. Keys in objects are not sorted but packs should contain only
+		// one key.
+		// So I guess we re safe.
+		var key = '';
+		for ( var a in pack) {
+			key = a;
+			break;
+		}
+
+		// Check if a load is running. If this is the case only add the callback
+		// function to be executed when the load completes.
+		if (this._cache.hasOwnProperty(key)) {
+			this._cache[key].callbackFns.push(fnOnComplete);
+			return;
+		}
+
+		// Add all files of this pack to our file list.
+		var keyList = pack[key].map(function(x) {
+			return x.key;
+		});
+
+		keyList.forEach(function(x) {
+			this._keyCache[x] = key;
+		}, this);
+
+		var countObj = {
+			key : key,
+			callbackFns : [ fnOnComplete ],
+			toLoad : 1,
+			type : 'file'
+		};
+		this._cache[key] = countObj;
+
+		this._cache[key].items = keyList;
+		this._cache[key].toLoad = keyList.length;
+
+		// Start to load all data in this pack.
+		this._loader.pack(key, null, pack);
+		this._loader.start();
+	}
+
+	load(data, fnOnComplete) {
+		if (Array.isArray(data)) {
+			console.error("Loading of arrays not yet supported.");
+			return;
+		}
+
+		fnOnComplete = fnOnComplete || NOOP;
+
+		// Temp.
+		var key = data.key;
+
+		// Check if a load is running. If this is the case only add the callback
+		// function to be executed when the load completes.
+		if (this._cache.hasOwnProperty(key)) {
+			this._cache[key].callbackFns.push(fnOnComplete);
+			return;
+		}
+
+		var countObj = {
+			key : key,
+			callbackFns : [ fnOnComplete ],
+			toLoad : 1,
+			type : 'file'
+		};
+
+		this._cache[key] = countObj;
+
+		switch (data.type) {
+		case 'json':
+			this._loader.json(data.key, data.url);
+			break;
+		default:
+			console.warn("Loading this type not supported: " + data.type);
+			return;
+		}
+
+		this._loader.start();
+	}
+
+}
---
+++ bestia-www-client/src/js/engine/core/DescriptionLoader.js
@@ -0,0 +1,84 @@
+import NOOP from '../../util/NOOP.js';
+
+/**
+ * This class is responsible for loading the description files of entities. It
+ * will determine from the context in which URL it needs to look. All loading
+ * operations are backed by the DemandLoader and thus are asynchronous.
+ */
+export default class DescriptionLoader {
+	constructor(loader, urlHelper) {
+		this._loader = loader;
+		this._url = urlHelper;
+
+	}
+
+	/**
+	 * Returns the description JSON file if it has already been loaded. Null
+	 * otherwise. The Name can be either the key name of the description file or
+	 * the data object. The DescriptionLoader will try to determine its name
+	 * then from the data object.
+	 * 
+	 * @param name
+	 */
+	getDescription(name) {
+		if((typeof name) !== 'string') {
+			name = this._getNameFromData(name) + '_desc';
+		}
+		
+		return this._loader.get(name, 'json');
+	}
+
+	/**
+	 * Loads the description file from the server. When the description was
+	 * loaded the callback function will be executed. It will get the
+	 * description file object as an argument.
+	 * 
+	 * @param {object}
+	 *            data - Object describing the description.
+	 * @param {function}
+	 *            fnCallback - Callback function. Executed when the data was
+	 *            loaded.
+	 */
+	loadDescription(data, fnCallback) {
+
+		fnCallback = fnCallback || NOOP;
+
+		var url = this._getUrlFromData(data);
+		var name = this._getNameFromData(data) + '_desc';
+
+		this._loader.load({
+			key : name,
+			type : 'json',
+			url : url
+		}, function() {
+			var descFile = this.getDescription(name);
+			fnCallback(descFile);
+		}.bind(this));
+	}
+
+	/**
+	 * Returns the right description URL depending on the data type.
+	 */
+	_getUrlFromData(data) {
+
+		switch (data.t) {
+		case 'MOB_ANIM':
+		case 'PLAYER_ANIM':
+			// its an mob.
+			return this._url.getMobDescUrl(data.s);
+		default:
+			// its an object.
+			return this._url.getObjectDescUrl(data.s);
+		}
+	}
+
+	/**
+	 * Returns the sprite name from the given data object.
+	 * 
+	 * @param {object}
+	 *            data
+	 */
+	_getNameFromData(data) {
+		return data.s;
+	}
+}
