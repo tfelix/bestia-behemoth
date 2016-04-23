c14a6d01a4b2248c037d92eee6ca7724e9c47a15
Thomas Felix <thomas.felix@tfelix.de>
2016-04-23 10:44:57 +0200

    Bumbed version auf 0.3.0

--- bestia-zoneserver/src/main/java/net/bestia/zoneserver/command/ecs/chat/ChatCommandExecutor.java
+++ bestia-zoneserver/src/main/java/net/bestia/zoneserver/command/ecs/chat/ChatCommandExecutor.java
@@ -2,8 +2,13 @@ package net.bestia.zoneserver.command.ecs.chat;
 
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.bestia.messages.ChatMessage;
 import net.bestia.messages.ChatMessage.Mode;
+import net.bestia.model.dao.AccountDAO;
+import net.bestia.model.domain.Account;
 import net.bestia.zoneserver.command.CommandContext;
 import net.bestia.zoneserver.proxy.PlayerEntityProxy;
 import net.bestia.zoneserver.util.PackageLoader;
@@ -15,6 +20,8 @@ import net.bestia.zoneserver.util.PackageLoader;
  *
  */
 public class ChatCommandExecutor {
+	
+	private static final Logger LOG = LogManager.getLogger(ChatCommandExecutor.class);
 
 	private final Set<ChatUserCommand> chatCommands;
 
@@ -30,11 +37,22 @@ public class ChatCommandExecutor {
 		if (msg.getChatMode() != Mode.COMMAND) {
 			return;
 		}
+		
+		final AccountDAO accDAO = ctx.getServiceLocator().getBean(AccountDAO.class);
+		
+		// Find the player who send the message.
+				final Account acc = accDAO.findOne(msg.getAccountId());
 
 		// Check if we have a suitable command.
 		for (ChatUserCommand cmd : chatCommands) {
 			if (msg.getText().startsWith(cmd.getChatToken())) {
-
+				
+				if (acc.getUserLevel().compareTo(cmd.getNeededUserLevel()) < 0) {
+					// User level not high enough.
+					LOG.debug("User level not high enough for command: {}, accId: {}", cmd.toString(), acc.getId());
+					return;
+				}
+				
 				cmd.execute(msg, player, ctx);
 
 			}
