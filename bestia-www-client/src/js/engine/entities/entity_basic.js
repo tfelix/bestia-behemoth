81d62a24d514bc2abf0d25f2484fb81fa6900902
tfelix <thomas.felix@tfelix.de>
2016-07-02 16:11:40 +0200

    An neue Syntax angepasst.

--- bestia-www-client/src/js/engine/entities/entity_basic.js
+++ bestia-www-client/src/js/engine/entities/entity_basic.js
@@ -1,132 +1,130 @@
-Bestia.Engine.BasicEntity = function(ctx, uuid) {
+
+/**
+ * Base entity for display via the bestia engine/phaser. The class contains all
+ * important position management function and methods.
+ */
+export default class Entity {
+	
+	constructor(ctx, uuid) {
+		/**
+		 * Position in tile coordinates.
+		 * 
+		 * @public
+		 * @property {Object}
+		 */
+		this._position = {
+			x : 0,
+			y : 0
+		};
+
+		/**
+		 * Entities have aswell a UUID with which they can be identified. This
+		 * UUID exisits (unlike the player bestia id, or pbid) in EVERY entity
+		 * which is spawned in the system.
+		 * 
+		 * @public
+		 * @property {String}
+		 */
+		this._uuid = uuid;
+
+		this._game = ctx.game;
+
+		this._ctx = ctx;
+
+		/**
+		 * The underlying sprite for the engine.
+		 * 
+		 * @public
+		 * @property {String}
+		 */
+		this._sprite = null;
+	}
+	
+	appear() {
+		// no op.
+	}
 
 	/**
-	 * Position in tile coordinates.
-	 * 
-	 * @public
-	 * @property {Object}
+	 * This will show and display the entity with the default appear animation.
 	 */
-	this._position = {
-		x : 0,
-		y : 0
-	};
+	show() {
+		// no op.
+	}
+
+	update() {
+		// no op.
+	}
 
 	/**
-	 * Entities have aswell a UUID with which they can be identified. This UUID
-	 * exisits (unlike the player bestia id, or pbid) in EVERY entity which is
-	 * spawned in the system.
-	 * 
-	 * @public
-	 * @property {String}
+	 * Shows death animation if any. The entity was killd/destroyed by
+	 * whatsoever means.
 	 */
-	this._uuid = uuid;
-
-	this._game = ctx.game;
-
-	this._ctx = ctx;
+	kill() {
+		// Placeholder death "animation".
+		this._sprite.tint = 0xffffff;
+		this._game.add.tween(this._sprite).to({alpha: 0}, 400, 'Linear', true);
+	}
 
 	/**
-	 * The underlying sprite for the engine.
+	 * This function is called every tick in the animation loop and can be used
+	 * to update internal sprite information. Especially in a multipart sprite
+	 * object this can be useful.
+	 */
+	tickAnimation() {
+		// no op.
+	}
+
+	/**
+	 * Removes an entity from the game.
 	 * 
 	 * @public
-	 * @property {String}
+	 * @method Bestia.Engine.BasicEntity#remove
 	 */
-	this._sprite = null;
-};
+	remove() {
 
-Bestia.Engine.BasicEntity.prototype.appear = function() {
-	// no op.
-};
+		if(this._sprite !== null) {
+			this._sprite.destroy();
+		}
 
-/**
- * This will show and display the entity with the default appear animation.
- */
-Bestia.Engine.BasicEntity.prototype.show = function() {
-	// no op.
-};
-
-Bestia.Engine.BasicEntity.prototype.update = function() {
-	// no op.
-};
-
-/**
- * Shows death animation if any. The entity was killd/destroyed by whatsoever
- * means.
- */
-Bestia.Engine.BasicEntity.prototype.kill = function() {
-	// Placeholder death "animation".
-	this._sprite.tint = 0xffffff;
-	this._game.add.tween(this._sprite).to({alpha: 0}, 400, 'Linear', true);
-};
-
-/**
- * This function is called every tick in the animation loop and can be used to
- * update internal sprite information. Especially in a multipart sprite object
- * this can be useful.
- */
-Bestia.Engine.BasicEntity.prototype.tickAnimation = function() {
-	// no op.
-};
-
-/**
- * Removes an entity from the game.
- * 
- * @public
- * @method Bestia.Engine.BasicEntity#remove
- */
-Bestia.Engine.BasicEntity.prototype.remove = function() {
-
-	this._sprite.destroy();
-
-};
-
-Bestia.Engine.BasicEntity.prototype.addToGroup = function(group) {
-	if (!(group instanceof Phaser.Group)) {
-		throw "Group must be instance of Phaser.Group";
 	}
 
-	if (this._sprite === null) {
-		console.warn('addToGroup: Sprite is still null. Was not loaded/set yet.');
-		return;
+	addToGroup(group) {
+		if (!(group instanceof Phaser.Group)) {
+			throw "Group must be instance of Phaser.Group";
+		}
+
+		if (this._sprite === null) {
+			console.warn('addToGroup: Sprite is still null. Was not loaded/set yet.');
+			return;
+		}
+
+		group.add(this._sprite);
 	}
 
-	group.add(this._sprite);
-};
+	_syncSpritePosition() {
+		// Correct the sprite position.
+		if (this._sprite !== null) {
+			var pos = Bestia.Engine.World.getSpritePxXY(this._position.x, this._position.y);
 
-Bestia.Engine.BasicEntity.prototype._syncSpritePosition = function() {
-	// Correct the sprite position.
-	if (this._sprite !== null) {
-		var pos = Bestia.Engine.World.getSpritePxXY(this._position.x, this._position.y);
-
-		this._sprite.x = pos.x;
-		this._sprite.y = pos.y;
+			this._sprite.x = pos.x;
+			this._sprite.y = pos.y;
+		}
 	}
-};
 
-Bestia.Engine.BasicEntity.prototype.setPosition = function(x, y, noSync) {
-	this._position.x = x;
-	this._position.y = y;
+	setPosition(x, y, noSync) {
+		this._position.x = x;
+		this._position.y = y;
 
-	if (!noSync) {
-		this._syncSpritePosition();
+		if (!noSync) {
+			this._syncSpritePosition();
+		}
 	}
-};
-
-Bestia.Engine.BasicEntity.prototype._getDistance = function(pos1, pos2) {
-	var x = pos1.x - pos2.x;
-	var y = pos1.y - pos2.y;
-
-	return Math.sqrt(x * x + y * y);
-};
-
-Object.defineProperty(Bestia.Engine.BasicEntity.prototype, 'position', {
-
-	get : function() {
+	
+	get position() {
 		return this._position;
-	},
-
-	set : function(value) {
-
+	}
+	
+	set position(value) {
 		value.x = value.x || 0;
 		value.y = value.y || 0;
 
@@ -134,41 +132,32 @@ Object.defineProperty(Bestia.Engine.BasicEntity.prototype, 'position', {
 		this._syncSpritePosition();
 	}
 
-});
-
-/**
- * Give access to the underlying sprite phaser API.
- */
-Object.defineProperty(Bestia.Engine.BasicEntity.prototype, 'sprite', {
-	get : function() {
+	/**
+	 * Give access to the underlying sprite phaser API.
+	 */
+	get sprite() {
 		return this._sprite;
 	}
-});
-
-/**
- * Readonly access to the uuid.
- */
-Object.defineProperty(Bestia.Engine.BasicEntity.prototype, 'uuid', {
-	get : function() {
+	
+	/**
+	 * Readonly access to the uuid.
+	 */
+	get uuid() {
 		return this._uuid;
 	}
-});
-
-/**
- * Returns the position in pixel in the world space.
- */
-Object.defineProperty(Bestia.Engine.BasicEntity.prototype, 'positionPixel', {
-
-	get : function() {
+	
+	/**
+	 * Returns the position of the entity in pixel in world space.
+	 */
+	get positionPx() {
 		return this._sprite.position;
-	},
-
-	set : function(value) {
-
+	}
+	
+	set positionPx(value) {
 		value.x = value.x || 0;
 		value.y = value.y || 0;
 
 		this._sprite.postion = value;
 	}
+}
 
-});
