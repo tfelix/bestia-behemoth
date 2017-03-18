8f88667ee51dc47198ba6f0f70a6d49050c43b33
Thomas Felix <thomas.felix@tfelix.de>
2017-03-18 14:00:17 +0100

    Cleaned up and improved the walkpeed object.

--- bestia-models/src/main/java/net/bestia/model/map/Walkspeed.java
+++ bestia-models/src/main/java/net/bestia/model/map/Walkspeed.java
@@ -5,17 +5,22 @@ package net.bestia.model.map;
  * (one encoded as float, the other as an int for transmission reason) the
  * walkspeed are completly encapsulated as a own class.
  * 
- * @TODO Testen.
- * 
  * @author Thomas Felix <thomas.felix@tfelix.de>
  *
  */
 public class Walkspeed {
 
-	private static final float MAX_WALKSPEED = 3.5f;
+	public static final float MAX_WALKSPEED = 3.5f;
+	public static final int MAX_WALKSPEED_INT = (int) (MAX_WALKSPEED * 100);
 
 	private float speed;
 
+	/**
+	 * Generates a walkspeed from a float value.
+	 * 
+	 * @param speed
+	 *            The current walkspeed in float.
+	 */
 	private Walkspeed(float speed) {
 		if (speed < 0 || speed > MAX_WALKSPEED / 1000) {
 
@@ -25,13 +30,21 @@ public class Walkspeed {
 	}
 
 	private Walkspeed(int speed) {
-		if (speed < 0 || speed > 3500) {
+		if (speed < 0 || speed > MAX_WALKSPEED_INT) {
 			throw new IllegalArgumentException("Walkspeed in int form must be between 0 and 3500");
 		}
 
 		this.speed = speed;
 	}
 
+	/**
+	 * Generates the walkspeed from an integer value. Value must be between 0
+	 * and {@link #MAX_WALKSPEED_INT}.
+	 * 
+	 * @param speed
+	 *            The speed.
+	 * @return A walkspeed object.
+	 */
 	public static Walkspeed fromInt(int speed) {
 		return new Walkspeed(speed);
 	}
@@ -40,6 +53,11 @@ public class Walkspeed {
 		return new Walkspeed(speed);
 	}
 
+	/**
+	 * Gets the current speed.
+	 * 
+	 * @return The current walkspeed.
+	 */
 	public float getSpeed() {
 		return speed;
 	}
@@ -47,9 +65,9 @@ public class Walkspeed {
 	/**
 	 * Returns the walkspeed as float.
 	 * 
-	 * @return
+	 * @return The current walkspeed as int.
 	 */
-	public float toInt() {
+	public int toInt() {
 		return (int) (speed * 100);
 	}
 
---
+++ bestia-models/src/test/java/net/bestia/model/map/WalkspeedTest.java
@@ -0,0 +1,60 @@
+package net.bestia.model.map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class WalkspeedTest {
+
+	@Test(expected=IllegalArgumentException.class)
+	public void fromInt_outOfRange_throws() {
+		Walkspeed.fromInt(23);
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void fromInt_negative_throws() {
+		Walkspeed.fromInt(-1);
+	}
+	
+	@Test
+	public void fromInt_ok() {
+		Walkspeed.fromInt(100);
+		Walkspeed.fromInt(0);
+		Walkspeed.fromInt(300);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void fromFloat_outOfRange_throws() {
+		Walkspeed.fromFloat(3.7f);
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void fromFloat_negative_throws() {
+		Walkspeed.fromFloat(-1.7f);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void fromFloat_ok() {
+		Walkspeed.fromFloat(1.7f);
+		Walkspeed.fromFloat(0f);
+		Walkspeed.fromFloat(Walkspeed.MAX_WALKSPEED);
+	}
+	
+	@Test
+	public void getSpeed_ok() {
+		Walkspeed ws = Walkspeed.fromFloat(Walkspeed.MAX_WALKSPEED);
+		Assert.assertEquals(0.01f, Walkspeed.MAX_WALKSPEED, ws.getSpeed());
+	}
+	
+	@Test
+	public void toInt_ok() {
+		Walkspeed ws = Walkspeed.fromFloat(Walkspeed.MAX_WALKSPEED);
+		Assert.assertEquals(3500, ws.toInt());
+		
+		ws = Walkspeed.fromFloat(0);
+		Assert.assertEquals(0, ws.toInt());
+		
+		ws = Walkspeed.fromInt(100);
+		Assert.assertEquals(100, ws.toInt());
+	}
+}
