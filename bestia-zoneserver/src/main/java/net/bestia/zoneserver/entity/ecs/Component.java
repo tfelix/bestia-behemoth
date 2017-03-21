fbcff924b40fc72fec60d8a1a8c5984028aa8fb0
Thomas Felix <thomas.felix@tfelix.de>
2017-03-21 23:59:06 +0100

    Improved the ECS entity.

--- bestia-zoneserver/src/main/java/net/bestia/zoneserver/entity/ecs/Component.java
+++ bestia-zoneserver/src/main/java/net/bestia/zoneserver/entity/ecs/Component.java
@@ -2,4 +2,7 @@ package net.bestia.zoneserver.entity.ecs;
 
 public interface Component {
 
+	static int getComponentId() {
+		throw new IllegalStateException("Must be impemented by child.");
+	}
 }
--- bestia-zoneserver/src/main/java/net/bestia/zoneserver/entity/ecs/Entity.java
+++ bestia-zoneserver/src/main/java/net/bestia/zoneserver/entity/ecs/Entity.java
@@ -1,22 +1,52 @@
 package net.bestia.zoneserver.entity.ecs;
 
 import java.io.Serializable;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-import net.bestia.zoneserver.entity.EntityContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.bestia.util.PackageLoader;
 
 public class Entity implements Serializable {
 
+	private static Logger LOG = LoggerFactory.getLogger(Entity.class);
+
 	private static final long serialVersionUID = 1L;
+	private static final int MAX_COMPONENT_ID;
+	static {
+		// we need to fing the max component id.
+		PackageLoader<Component> loader = new PackageLoader<>(Component.class, "net.bestia.zoneserver.entity.ecs");
+
+		int maxId = 0;
+		for (Class<? extends Component> clazz : loader.getSubClasses()) {
+			try {
+				Method idMethod = clazz.getDeclaredMethod("getComponentId");
+				Integer id = (Integer) idMethod.invoke(null);
+
+				if (id > maxId) {
+					maxId = id;
+				}
+			} catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+				LOG.error("Could not get the ID of the component.", e);
+			}
+		}
+
+		MAX_COMPONENT_ID = maxId;
+	}
+
 	private long id = -1;
-	
+	private Component[] components = new Component[MAX_COMPONENT_ID];
+
 	/**
 	 * This will set an id of -1.
 	 */
 	public Entity(long id) {
-		
+
 		this.id = id;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
