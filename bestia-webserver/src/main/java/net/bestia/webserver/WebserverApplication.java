6a5bb1227a6f37f5a94abfbf7dbf854dd791d886
Thomas Felix <thomas.felix@tfelix.de>
2017-03-28 00:34:20 +0200

    Removed unneeded hibernate datasource references.

--- bestia-webserver/src/main/java/net/bestia/webserver/WebserverApplication.java
+++ bestia-webserver/src/main/java/net/bestia/webserver/WebserverApplication.java
@@ -2,10 +2,7 @@ package net.bestia.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Main entry point of the webserver application.
@@ -14,9 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  *
  */
 @SpringBootApplication
-@EnableJpaRepositories("net.bestia.model")
-@EntityScan("net.bestia.model.domain")
-@ComponentScan(basePackages = { "net.bestia.model.service", "net.bestia.webserver" })
 public class WebserverApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
--- bestia-webserver/src/main/java/net/bestia/webserver/controller/AccountController.java
+++ bestia-webserver/src/main/java/net/bestia/webserver/controller/AccountController.java
@@ -1,15 +1,10 @@
 package net.bestia.webserver.controller;
 
-import java.util.Objects;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.bestia.messages.web.AccountLoginToken;
-import net.bestia.model.domain.Account;
-import net.bestia.model.service.AccountService;
 import net.bestia.webserver.exceptions.WrongCredentialsException;
 
 /**
@@ -21,27 +16,17 @@ import net.bestia.webserver.exceptions.WrongCredentialsException;
 @RestController("account/")
 public class AccountController {
 
-	private final AccountService accountService;
-
-	@Autowired
-	public AccountController(AccountService accService) {
-
-		this.accountService = Objects.requireNonNull(accService);
-	}
 
 	@RequestMapping("login")
 	public AccountLoginToken login(
 			@RequestParam(value = "accName") String account,
 			@RequestParam(value = "password") String password) {
 
-		final Account acc = accountService.createLoginToken(account, password);
+		throw new WrongCredentialsException();
 
-		if (acc == null) {
-			throw new WrongCredentialsException();
-		}
-
-		final AccountLoginToken creds = new AccountLoginToken(acc.getId(), acc.getLoginToken(),
-				acc.getMaster().getName());
-		return creds;
+		/*
+		 * final AccountLoginToken creds = new AccountLoginToken(acc.getId(),
+		 * acc.getLoginToken(), acc.getMaster().getName()); return creds;
+		 */
 	}
 }
