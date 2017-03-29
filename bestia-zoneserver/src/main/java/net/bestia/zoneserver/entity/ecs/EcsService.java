30e5dc8c4de79d06496e5eb7f4911dcb4e57201b
Thomas Felix <thomas.felix@tfelix.de>
2017-03-29 23:57:41 +0200

    Worked on ECS system.

--- bestia-zoneserver/src/main/java/net/bestia/zoneserver/entity/ecs/EcsService.java
+++ bestia-zoneserver/src/main/java/net/bestia/zoneserver/entity/ecs/EcsService.java
@@ -1,15 +1,51 @@
 package net.bestia.zoneserver.entity.ecs;
 
+import java.util.Collection;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
 import com.hazelcast.core.MultiMap;
 
 @Service
 public class EcsService {
 
-	private MultiMap<Long, Component> entityComponents;
+	private final static String ECS_ENTITY_MAP = "entities.ecs";
+	private final static String ENTITIES_MAP = "entities";
+
+	private final IMap<Long, Entity> entities;
+	private final MultiMap<Long, Component> entityComponents;
+
+	public EcsService(HazelcastInstance hz) {
+		Objects.requireNonNull(hz);
+
+		entityComponents = hz.getMultiMap(ECS_ENTITY_MAP);
+		entities = hz.getMap(ENTITIES_MAP);
+	}
+
+	public <T> Optional<T> getComponent(long entityId, Class<T> clazz) {
+
+		return entityComponents.get(entityId)
+				.stream()
+				.filter(c -> clazz.isInstance(c))
+				.map(c -> clazz.cast(c))
+				.findFirst();
+	}
+	
+	public void addComponent(long entityId, Component comp) {
+		throw new IllegalStateException();
+	}
+	
+	public void removeComponent(long entityId, Component comp) {
+		throw new IllegalStateException();
+	}
+	
+	public Collection<Entity> getEntitiesWithComponent(Class<Component>... compClass) {
+		throw new IllegalStateException();
+	}
 
 	/**
 	 * 
@@ -25,14 +61,5 @@ public class EcsService {
 				.findFirst()
 				.map(x -> true)
 				.orElse(false);
-	}
-	
-	public <T> Optional<T> getComponent(Entity e, Class<T> comp) {
-
-		return  entityComponents.get(e.getId())
-				.stream()
-				.filter(c -> comp.isInstance(c))
-				.map(c -> comp.cast(c))
-				.findFirst();
 	}
 }
