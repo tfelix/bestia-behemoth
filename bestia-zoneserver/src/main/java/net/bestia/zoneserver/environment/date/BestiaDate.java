2d2570a77de3d0ef87a9fe185fd12eb5aa9b7b3b
Thomas Felix <thomas.felix@tfelix.de>
2017-04-02 16:48:44 +0200

    Improved the bestia time date model.

--- bestia-zoneserver/src/main/java/net/bestia/zoneserver/environment/date/BestiaDate.java
+++ bestia-zoneserver/src/main/java/net/bestia/zoneserver/environment/date/BestiaDate.java
@@ -1,24 +1,72 @@
 package net.bestia.zoneserver.environment.date;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
+import java.util.Date;
+import java.util.Objects;
 
 public class BestiaDate {
 
 	private final LocalDateTime startDate;
 
-	private final static int DAY_HOURS = 16;
 	private final static int HOUR_MINUTES = 60;
+	private final static int DAY_HOURS = 16;
+	private final static int MONTH_DAYS = 30;
 	private final static int YEAR_MONTHS = 8;
 
-	public BestiaDate(TemporalAccessor temporal) {
-		startDate = LocalDateTime.from(temporal);
-	}
-	
+	private final static int MINUTES_OF_DAY = HOUR_MINUTES * DAY_HOURS;
+	private final static int MINUTES_OF_MONTH = MONTH_DAYS * MINUTES_OF_DAY;
+	private final static int MINUTES_OF_YEAR = YEAR_MONTHS * MINUTES_OF_MONTH;
+
+	/**
+	 * Creates a new date object with the starting time set to now.
+	 */
 	public BestiaDate() {
-		startDate = LocalDateTime.now();
+		this(LocalDateTime.now());
+		// no op.
+	}
+
+	private BestiaDate(LocalDateTime time) {
+		startDate = Objects.requireNonNull(time);
+	}
+
+	/**
+	 * Creates a bestia date from the given {@link java.util.Date} object which
+	 * is typically stored inside the database.
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static BestiaDate fromDate(Date date) {
+
+		final Instant instant = date.toInstant();
+		final LocalDateTime time = instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+		return new BestiaDate(time);
+	}
+
+	/**
+	 * Returns the progress percentage of the day. Starting at 0 o'clock a the
+	 * night which is then 0 and 24 o'clock which is given as 1.0.
+	 * 
+	 * @return The current progress of the day.
+	 */
+	public float getDayProgress() {
+		final LocalDateTime now = LocalDateTime.now();
+		return (Duration.between(startDate, now).toMinutes() % MINUTES_OF_DAY) / (float) MINUTES_OF_DAY;
+	}
+
+	public float getMonthProgress() {
+		final LocalDateTime now = LocalDateTime.now();
+		return (Duration.between(startDate, now).toMinutes() % MINUTES_OF_MONTH) / (float) MINUTES_OF_MONTH;
+	}
+
+	public float getYearProgress() {
+		final LocalDateTime now = LocalDateTime.now();
+		return (Duration.between(startDate, now).toMinutes() % MINUTES_OF_YEAR) / (float) MINUTES_OF_YEAR;
 	}
 
 	/**
--- bestia-zoneserver/src/test/java/net/bestia/zoneserver/BestiaDateTest.java
+++ bestia-zoneserver/src/test/java/net/bestia/zoneserver/BestiaDateTest.java
@@ -3,7 +3,9 @@ package net.bestia.zoneserver;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Calendar;
+import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import net.bestia.zoneserver.environment.date.BestiaDate;
@@ -11,7 +13,7 @@ import net.bestia.zoneserver.environment.date.BestiaDate;
 public class BestiaDateTest {
 	
 	@Test
-	public void correct_hours() {
+	public void getHours_correctHours() {
 		BestiaDate d = new BestiaDate();
 		Calendar cal = Calendar.getInstance();
 		int hour = cal.get(Calendar.HOUR_OF_DAY) % 8;
@@ -19,15 +21,21 @@ public class BestiaDateTest {
 	}
 	
 	@Test
-	public void correct_minutes() {
+	public void getMinutes_correctMinutes() {
 		BestiaDate d = new BestiaDate();
 		Calendar cal = Calendar.getInstance();
 		assertEquals(d.getMinutes(), cal.get(Calendar.MINUTE));
 	}
 
 	@Test
-	public void correct_season() {
-		// TODO
+	public void fromDate_instance() {
+		BestiaDate bd = BestiaDate.fromDate(createDate());
+		Assert.assertNotNull(bd);
 	}
-
+	
+	private Date createDate() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2016, 3, 12, 13, 00);
+		return cal.getTime();
+	}
 }
