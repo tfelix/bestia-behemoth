b8fb6817f86e422efcb73464cd0a762ab55cd6d9
Thomas Felix <thomas.felix@tfelix.de>
2017-04-16 20:12:30 +0200

    Added unit tests and added WalkAnimController.

---
+++ bestia-www-client/src/js/engine/animation/SpriteAnimationCache.js
@@ -0,0 +1,31 @@
+
+/**
+ * This class holds globally all sprite animation data of instanced sprites. It should be asked
+ * if a sprite wants to perform an animation this cache is asked if the animation does exist.
+ * If the sprite description is loaded the first time the description object is feed into the 
+ * SpriteAnimationCache and the data is parsed into a usable format.
+ * If there is an animation requested which does not exist it will return an alternative usable
+ * animation name.
+ */
+export default class SpriteAnimationManager {
+
+	/**
+	 * Adds the given sprite description to the manager and parses its data to a better usable
+	 * format for the manager.
+	 * 
+	 * @public
+	 * @param {object} desc - Description for the sprite object.
+	 */
+	addSpriteDescription(desc) {
+		throw 'implement';
+	}
+
+	/**
+	 * Clears and resets the cache in its original state thus deleting all the cached data.
+	 * 
+	 * @public
+	 */
+	clear() {
+		throw 'implement';
+	}
+}
---
+++ bestia-www-client/test/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "env": {
+        "mocha": true,
+        "es6": true
+    }
+};
