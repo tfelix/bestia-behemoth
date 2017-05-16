1b94384956dd162a9f2d63b4f137a9f073b8bc05
Thomas Felix <thomas.felix@tfelix.de>
2017-05-16 20:50:36 +0200

    Added script resolver.

---
+++ bestia-zoneserver/src/main/java/net/bestia/zoneserver/script/ScriptFileResolver.java
@@ -0,0 +1,57 @@
+package net.bestia.zoneserver.script;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class looks up a path for a script file. It is determined by the name of
+ * the script and the type of the script.
+ * 
+ * @author Thomas Felix
+ *
+ */
+public class ScriptFileResolver {
+
+	private final static Logger LOG = LoggerFactory.getLogger(ScriptFileResolver.class);
+
+	private final ClassLoader classLoader = getClass().getClassLoader();
+
+	public ScriptFileResolver() {
+		// no op.
+	}
+
+	/**
+	 * Returns the global script file which contains helper and API access
+	 * helper.
+	 * 
+	 * @return The global script file.
+	 */
+	public File getGlobalScriptFile() {
+		final File globalScriptFile = new File(classLoader.getResource("script/globalContext.js").getFile());
+		LOG.debug("Getting global script file: {}", globalScriptFile.getAbsolutePath());
+		return globalScriptFile;
+	}
+
+	/**
+	 * Returns the script file path for the path and the type.
+	 * 
+	 * @param name
+	 *            The name of the script.
+	 * @param type
+	 *            The script type.
+	 * @return The path to the script.
+	 */
+	public File getScriptFile(String name, ScriptType type) {
+
+		if (!name.endsWith(".js")) {
+			name += ".js";
+		}
+
+		final String endPath = String.format("script/%s/%s", type.toString().toLowerCase(), name);
+		final File scriptFile = new File(classLoader.getResource(endPath).getFile());
+		LOG.debug("Getting global script file: {}", scriptFile.getAbsolutePath());
+		return scriptFile;
+	}
+}
---
+++ bestia-zoneserver/src/test/java/net/bestia/zoneserver/script/ScriptFileResolverTest.java
@@ -0,0 +1,32 @@
+package net.bestia.zoneserver.script;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ScriptFileResolverTest {
+
+	private ScriptFileResolver resolver = new ScriptFileResolver();
+
+	@Test
+	public void getGlobalScriptFile_ok() {
+		Assert.assertTrue(resolver.getGlobalScriptFile().toString().endsWith("globalContext.js"));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void getScriptFile_nullScriptName_throws() {
+		resolver.getScriptFile(null, ScriptType.ITEM);
+	}
+
+	@Test
+	public void getScriptFile_scriptNameAndType_works() {
+		String scriptFile = resolver.getScriptFile("test.js", ScriptType.ATTACK).getAbsolutePath();
+		Assert.assertTrue(scriptFile.endsWith("attack/test.js"));
+	}
+	
+	@Test
+	public void getScriptFile_scriptNameWithoutJs_works() {
+		String scriptFile = resolver.getScriptFile("test", ScriptType.ATTACK).getAbsolutePath();
+		Assert.assertTrue(scriptFile.endsWith("item/test.js"));
+	}
+	
+}
