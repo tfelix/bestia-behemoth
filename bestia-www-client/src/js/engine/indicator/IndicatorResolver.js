95141a04b7705d03d3b892db8c251b513daec009
Thomas Felix <thomas.felix@tfelix.de>
2017-09-29 00:25:59 +0200

    Some comments.

---
+++ bestia-www-client/src/js/engine/indicator/IndicatorResolver.js
@@ -0,0 +1,13 @@
+
+/**
+ * Depending on the tags of the entity the system will resolve the on 
+ * mouse over to a certain indicator. This indicator will be requested 
+ * by the system.
+ */
+export default class IndicatorResolver {
+
+    constructor() {
+
+    }
+
+}
