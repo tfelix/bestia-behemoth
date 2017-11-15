567815331ca79f50d93e752d7b97563fe35483f4
Thomas Felix <thomas.felix@tfelix.de>
2017-11-15 20:36:50 +0100

    Started work with intelliJ optimizing component handling.

--- bestia-webserver-ex/src/main/java/net/bestia/webserver/ClusterConnectionTerminated.java
+++
@@ -1,5 +0,0 @@
-package net.bestia.webserver;
-
-public class ClusterConnectionTerminated {
-
-}
--- bestia-zoneserver/src/main/java/net/bestia/messages/internal/FromClient.java
+++ bestia-zoneserver/src/main/java/net/bestia/messages/internal/FromClient.java
@@ -2,6 +2,9 @@ package net.bestia.messages.internal;
 
 import net.bestia.messages.AccountMessage;
 
+/**
+ * Message coming from the client sometimes are redundant and thus need to me marked to they can get routed the right way.
+ */
 public class FromClient extends AccountMessage {
 
 	private static final long serialVersionUID = 1L;
