f8a236254f26cbd435a8557b0129eb717fd700b2
Thomas Felix <thomas.felix@tfelix.de>
2018-01-18 22:46:27 +0100

    Revert "Cleanup"
    This reverts commit 33effe930f2afd0be591043246db1398a065657a.

--- bestia-zoneserver/src/main/java/net/bestia/entity/factory/package-info.java
+++
@@ -1,9 +0,0 @@
-
-/**
- * This package contains the factories which are used to create any kind of
- * entities in the game.
- * 
- * @author Thomas Felix
- *
- */
-package net.bestia.entity.factory;
--- bestia-zoneserver/src/test/java/net/bestia/entity/factory/EntityFactory2Test.java
+++
@@ -1,5 +0,0 @@
-package net.bestia.entity.factory;
-
-class EntityFactory2Test {
-
-}
