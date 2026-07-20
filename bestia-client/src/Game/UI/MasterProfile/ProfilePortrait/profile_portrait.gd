dc96b33bd63dd024ce19aab0d66382881f5ce6db
Thomas Felix <thomas.felix@tfelix.de>
2026-07-20 19:31:35 +0200

    Portrait now does not bleed anymore into the game

--- bestia-client/src/Game/UI/MasterProfile/ProfilePortrait/profile_portrait.gd
+++ bestia-client/src/Game/UI/MasterProfile/ProfilePortrait/profile_portrait.gd
@@ -1,7 +1,10 @@
 extends TextureRect
 
 @onready var _master_visual: MasterVisual = %MasterVisual
-
+@onready var _viewport: Viewport = %PortraitViewport
 
 func apply_visual(msg: MasterVisualComponentSMSG) -> void:
 	_master_visual.setup_visual(msg)
+	# Render it once again after the model has been updated.
+	await get_tree().process_frame
+	_viewport.render_target_update_mode = SubViewport.UPDATE_ONCE
